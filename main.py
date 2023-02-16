5eaaff93ead36f9c52aa5d81a987d441e25d1c26
gitgudsec <moi@faanross.com>
2023-02-16 18:28:27 -0500

    python api

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# this is called a path operation
+# this is called a path operation - decorator + function
+
 # decorator @app.get("/"): this is what takes it from simply being a function to an API
 # we use app (from line 3) and then specify to use a GET method, we can use others too - POST etc
 # we also specify the path ("/"), which in this case refers to the root path
-# function root():
+
+# function root(): note that we use a dictionary, but fastAPI automatically converts this to JSON
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
-
-# note that we use a dictionary, but fastAPI automatically converts this to JSON
+    return {"message": "What's the word, ma NURD."}
