69617b763866a699fbc8da43b7053890fbf66bac
gitgudsec <moi@faanross.com>
2023-02-16 18:27:10 -0500

    python api

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # this is called a path operation
-# decorator @app.get("/")
+# decorator @app.get("/"): this is what takes it from simply being a function to an API
+# we use app (from line 3) and then specify to use a GET method, we can use others too - POST etc
+# we also specify the path ("/"), which in this case refers to the root path
 # function root():
 
 @app.get("/")
