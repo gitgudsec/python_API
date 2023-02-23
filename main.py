ad485df8ff8349c11332b5ce360b1d81205998a0
Faan Ross <moi@faanross.com>
2023-02-23 16:44:59 -0500

    python api

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-# from fastapi import FastAPI
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
-# app = FastAPI()
+app = FastAPI()
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
+class Post(BaseModel):
+    title: str
+    content: str
+    id: int
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "numbchuc Skillz", "content": "Pretty good with a bow", "id": 69}]
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+@app.post("/posts")
+def create_post(post: Post):
+    print(post.dict())
+    print(post)
+    return {"data": "Chapstick"}
